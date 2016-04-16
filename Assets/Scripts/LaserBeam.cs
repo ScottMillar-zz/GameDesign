f21e0d0736e93b79c5baab3e7005ebe29006beab
Scott Millar <scottmillar95@Gmail.com>
2016-04-16 17:57:51 +0100

    Initial commit

---
+++ Assets/Scripts/LaserBeam.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LaserBeam : MonoBehaviour {
+
+    public float range = 100f;
+
+    Ray shootRay;
+    RaycastHit laserHit;
+    LineRenderer laserLine;
+    int shootableMask;
+
+    void Start ()
+    {
+
+        laserLine = GetComponent<LineRenderer>();
+
+    }
+	
+	
+	void Update ()
+    {
+
+        shootableMask = LayerMask.GetMask("Shootable");
+
+        laserLine.enabled = true;
+        laserLine.SetPosition(0, transform.position);
+
+        shootRay.origin = transform.position;
+        shootRay.direction = transform.forward;
+
+        if (Physics.Raycast(shootRay, out laserHit, range, shootableMask))
+        {
+            laserLine.SetPosition(1, laserHit.point);
+        }
+
+        else
+        {
+            laserLine.SetPosition(1, shootRay.origin + shootRay.direction * range);
+        }
+    }
+}
