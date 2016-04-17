2ed61bfd62b10926506bf19c6b2af4b9b064e68c
Scott Millar <scottmillar95@gmail.com>
2016-04-17 01:41:00 +0100

    sucka dick

--- Assets/Scripts/LaserBeam.cs
+++ Assets/Scripts/LaserBeam.cs
@@ -4,19 +4,18 @@ using System.Collections;
 public class LaserBeam : MonoBehaviour {
 
     public float range = 100f;
+    public bool isAND = false;
     public GameObject laserCollision;
-
-    Ray shootRay;
-    
-    RaycastHit laserHit;
+    public GameObject laserParticle;
+    public RaycastHit laserHit;
+    public Ray shootRay;
     LineRenderer laserLine;
+    bool mfw;
     int shootableMask;
 
     void Start ()
     {
-
         laserLine = GetComponent<LineRenderer>();
-        Instantiate(laserCollision, laserHit.point, transform.rotation);
     }
 	
 	
@@ -25,20 +24,46 @@ public class LaserBeam : MonoBehaviour {
 
         shootableMask = LayerMask.GetMask("Shootable");
 
-        laserLine.enabled = true;
+        if( isAND == false)
+        {
+            DoTheLaser();
+        }
+       
+    }
+
+    void DoTheLaser ()
+    {
+        laserLine.enabled = true;    
         laserLine.SetPosition(0, transform.position);
+
+        laserParticle.SetActive(true);
 
         shootRay.origin = transform.position;
         shootRay.direction = transform.forward;
 
+
         if (Physics.Raycast(shootRay, out laserHit, range, shootableMask))
         {
             laserLine.SetPosition(1, laserHit.point);
-        }
 
+            if (laserHit.collider.tag == "AND")
+            {
+                mfw = laserHit.collider.gameObject.GetComponent<RayDetection>().detectedRays[0];
+
+
+                laserHit.transform.SendMessage("HitByRay");
+            }
+
+        }
         else
         {
             laserLine.SetPosition(1, shootRay.origin + shootRay.direction * range);
         }
+    }
+
+    //gonna use arrays to do the do
+    void sendPower ()
+    {
+
     }
 }
---
+++ Assets/Scripts/LaserCollision.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LaserCollision : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = GetComponentInParent<LaserBeam>().laserHit.point;
+        transform.LookAt(transform.parent.parent.position);
+    }
+}
